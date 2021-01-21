f8294f99478f6ceaac8f05e4b80bf83b11c33194
kmualim <kristy.mualim@mail.mcgill.ca>
2021-01-21 04:40:17 -0800

    update alt_tss_annotation code

---
+++ alt_tss_annotation/getTSS.py
@@ -0,0 +1,17 @@
+#! bin/python3
+
+import pandas as pd
+import numpy as np
+import os,sys
+
+FILE=sys.argv[1]
+
+data = pd.read_csv(FILE, sep="\t", header=None)
+
+positives = data.loc[data[5]=="+"].index.astype('int')
+negatives = data.loc[data[5]=="-"].index.astype('int')
+
+data.iloc[positives,2] = data.iloc[positives, 1]
+data.iloc[negatives,1] = data.iloc[negatives, 2]
+
+data.to_csv(str(FILE)+".TSS.tmp", sep="\t", index=False, header=False)
